0336569d6ddf91b987570432591096601bdd144c
Amanda Gonsalves <amandagonsalves75@gmail.com>
2020-12-22 23:30:33 -0300

    update

--- backend/src/api/dataCases.js
+++ backend/src/api/dataCases.js
@@ -1,4 +1,4 @@
-const { connections } = require('mongoose');
+const moment = require('moment');
 const unirest = require('unirest');
 const cases = require('./case');
 const Case = require('./case');
@@ -34,15 +34,42 @@ const insertData = async () => {
 
   for (let data of dataJSON) {
     const search = await cases.find({ body: { $exists: true, $eq: data } }).countDocuments();
-     
+
     if (search === 0) {
       saveCase(data);
     }
-    
+
     return false;
   }
 }
 
+const createDatesArray = (date, days) => {
+  const dateArr = [];
+
+  let prevDate = moment(date).subtract(days, 'days').format('MM-DD-YYYY');
+  let last = moment(date).format('MM-DD-YYYY');
+
+  let start = new Date(prevDate);
+  let end = new Date(last);
+
+  while (start < end) {
+    dateArr.push(moment(start).format('yyyy-mm-ddThh:mm:ss+zzzzzz'));
+
+    let newDate = start.setDate(start.getDate() + 1);
+    start = new Date(newDate);
+  }
+
+  return dateArr;
+}
+
+const getDataFromTheLastDays = async (country, date) => {
+  const getDataByCountry = await cases.find({ 'body.name': country , 'body.reportDate': date });
+  
+  console.log(getDataByCountry);
+}
+
+getDataFromTheLastDays('Saint Pierre and Miquelon', '2020-08-16T00:00:00.000Z');
+
 module.exports = {
   insertData
-};
+};  
