c0e5f59fa7bd8f4b87900ab665dcf09324d04f20
Amanda Gonsalves <amandagonsalves75@gmail.com>
2021-01-04 15:04:31 -0300

    update

--- frontend/src/layout/getData.js
+++ frontend/src/layout/getData.js
@@ -45,7 +45,7 @@ const getTotalPerRegion = (data, field) => {
   });
 }
 
-const getValues = (list, field, valueY, dep) => {
+const getValues = (list, field, valueY) => {
   const regions = getFields(list, 'region');
   const item = getTotalPerRegion(list, field);
 
